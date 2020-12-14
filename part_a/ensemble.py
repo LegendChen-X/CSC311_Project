fe04482a53e36068f5933dd3fd245ab49b5faffd
Xiang <chenxiang00825@gmail.com>
2020-12-13 19:11:52 -0500

    part a 4

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,7 +1,6 @@
 # TODO: complete this file.
 from knn import *
 from item_response import *
-from neural_network import *
 import numpy as np
 import torch
 
@@ -9,45 +8,49 @@ def sample(data):
     indexes = np.random.randint(len(data["user_id"]), size=len(data["user_id"]))
     sample = {"user_id":data["user_id"][indexes], "question_id":data["question_id"][indexes], "is_correct":data["is_correct"][indexes]}
     return sample
+    
+def newEvaluate(data, theta, beta):
+    """ Evaluate the model given data and return the accuracy.
+    :param data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
 
-def sampleMat(matrix):
-    indexes = np.random.randint(matrix.shape[0], size=matrix.shape[0])
-    sampleMat = matrix[indexes, :]
-    return sampleMat
-
-def knn(sampleMat, validData):
+    :param theta: Vector
+    :param beta: Vector
+    :return: float
+    """
+    pred = []
+    for i, q in enumerate(data["question_id"]):
+        u = data["user_id"][i]
+        x = (theta[u] - beta[q]).sum()
+        p_a = sigmoid(x)
+        pred.append(p_a)
+    return pred
+    
+def bag(trainData, validData, iter):
     res = []
-    nbrs = KNNImputer(n_neighbors=11)
-    mat = nbrs.fit_transform(sampleMat)
-    for i in len(validData["user_id"]):
-        res.append()
-    return res
-    
-def nn(sample, validData):
-    zero_train_matrix, train_matrix, valid_data, test_data = load_data()
-    k = 50
-    lr = 0.05
-    num_epoch = 10
-    lamb = 0.01
-    model = AutoEncoder(sample.shape[1], k)
-    train(model, lr, lamb, train_matrix, zero_train_matrix, valid_data, num_epoch, 0)
-    
-def bag(trainData, validData):
-    knn_res = knn(sample(trainData), validData)
-    ir_res = ir(sample(trainData), validData)
-    nn_res = nn(sample(trainData), validData)
-    final_res = (knn_res + ir_res + nn_res) / 3
-    predict = np.zereos((len(final_res), ), dtype="float")
-    for i in range(len(final_res)):
-        if i < 0.5: predict[i] = 0
+    for i in range(iter):
+        sampleData = sample(trainData)
+        best_theta, best_beta, val_acc_lst, train_neg_lld_lst, valid_neg_lld_lst = irt(sampleData, validData, 0.002, 250)
+        res.append(newEvaluate(validData, best_theta, best_beta))
+    prob = np.zeros((len(validData["is_correct"]),), dtype="float")
+    for i in range(validData["is_correct"]):
+        total = 0
+        for j in range(iter): total += res[j][i]
+        prob[i] = total / iter
+    predict = np.zeros((len(validData["is_correct"]),), dtype="float")
+    for i in range(len(predict)):
+        if prob[i] < 0.5: predict[i] = 0
         else: predict[i] = 1
+    return np.sum((validData["is_correct"] == predict)) / len(validData["is_correct"])
     
 def main():
+    print("cxnb")
     train_data = load_train_csv("../data")
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
-    print(len(val_data["is_correct"]))
-    print(len(val_data["user_id"]))
-    print(len(valid_data["question_id"]))
+    print(bag(train_data, val_data, 3))
+    
+if __name__ == "__main__":
+    main()
     
     
